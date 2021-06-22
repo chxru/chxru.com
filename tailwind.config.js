5b47081b365b2043548864b95e81c072ce1a4ab9
Charuka Samarakoon <charuka@protonmail.com>
2021-06-22 18:54:04 +0530

    Basic Structure init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: '#f8f8f8',
+        primary: '#2e3a59'
+      },
+      fontFamily: {
+        'body': ['Roboto', 'sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
